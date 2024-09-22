864a801ff0732a4c13e9f3b0433e39897088d796
Ethan Burd <54852139+burdbrains@users.noreply.github.com>
2024-09-22 09:54:14 -0500

    Utilizing bucket for index.html

--- iac-lab1/__main__.py
+++ iac-lab1/__main__.py
@@ -2,7 +2,14 @@
 
 import pulumi
 import pulumi_aws as aws
+import os
 
 # "my-s3-bucket" is called the 'logical name'
-bucket = aws.s3.Bucket("my-s3-bucket")
-pulumi.export("bucket_name", bucket.bucket)
+bucket = aws.s3.Bucket('my-s3-bucket')
+filepath = os.path.join('site', 'index.html')
+obj = aws.s3.BucketObject('index.html',
+                          bucket=bucket.bucket,
+                          source=pulumi.FileAsset(filepath)
+                          )
+
+pulumi.export('bucket_name', bucket.bucket)
