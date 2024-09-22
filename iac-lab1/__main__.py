4dd3b61a85926a3b43116c50ea28eb967a9dc991
Ethan Burd <54852139+burdbrains@users.noreply.github.com>
2024-09-22 17:05:21 -0500

    Successfully deploying a static website with pulumi.

--- iac-lab1/__main__.py
+++ iac-lab1/__main__.py
@@ -3,13 +3,48 @@
 import pulumi
 import pulumi_aws as aws
 import os
+import mimetypes
 
 # "my-s3-bucket" is called the 'logical name'
-bucket = aws.s3.Bucket('my-s3-bucket')
-filepath = os.path.join('site', 'index.html')
+# example = aws.s3.BucketV2("example", bucket="my-tf-example-bucket")
+new_bucket = aws.s3.BucketV2('new-bucket', bucket='my-s3-new-bucket')
+
+new_bucket_website = aws.s3.BucketWebsiteConfigurationV2('new_bucket',
+                                                        bucket=new_bucket.id,
+                                                        index_document={
+                                                            'suffix': 'index.html'
+                                                        })
+
+new_bucket_ownership_controls = aws.s3.BucketOwnershipControls('my-s3-bucket',
+    bucket=new_bucket.id,
+    rule={
+        'object_ownership': 'BucketOwnerPreferred',
+    })
+
+new_bucket_public_access = aws.s3.BucketPublicAccessBlock('my-s3-bucket',
+                                                      bucket=new_bucket.id,
+                                                      block_public_acls=False,
+                                                      block_public_policy=False,
+                                                      ignore_public_acls=False,
+                                                      restrict_public_buckets=False
+                                                      )
+
+new_bucket_acl = aws.s3.BucketAclV2('my-s3-bucket',
+                                bucket=new_bucket.id,
+                                acl='public-read',
+                                opts=pulumi.ResourceOptions(depends_on=[
+                                    new_bucket_public_access,
+                                    new_bucket_ownership_controls
+                                ]))
+
+filepath = os.path.join('sites', 'index.html')
+mime_type, _unneeded = mimetypes.guess_type(filepath)
 obj = aws.s3.BucketObject('index.html',
-                          bucket=bucket.bucket,
-                          source=pulumi.FileAsset(filepath)
+                          bucket=new_bucket.id,
+                          source=pulumi.FileAsset(filepath),
+                          acl="public-read",
+                          content_type=mime_type
                           )
 
-pulumi.export('bucket_name', bucket.bucket)
+pulumi.export('new_bucket_name', new_bucket.bucket)
+pulumi.export('new_bucket_endpoint', pulumi.Output.concat('http://', new_bucket_website.website_endpoint))
