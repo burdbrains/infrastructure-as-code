2486c4d62246733e01e43447a928cb3645fecd4e
Ethan Burd <54852139+burdbrains@users.noreply.github.com>
2024-09-23 08:29:01 -0500

    Completed lab1, buckets are now dynamically created by stack, so theres no 'BucketAlreadyOwnedByYou' Error.

--- iac-lab1/__main__.py
+++ iac-lab1/__main__.py
@@ -5,23 +5,21 @@ import pulumi_aws as aws
 import os
 import mimetypes
 
+config = pulumi.Config()
+site_dir = config.require('siteDir')
+stack = pulumi.get_stack()
+
 # "my-s3-bucket" is called the 'logical name'
 # example = aws.s3.BucketV2("example", bucket="my-tf-example-bucket")
-new_bucket = aws.s3.BucketV2('new-bucket', bucket='my-s3-new-bucket')
+new_bucket = aws.s3.BucketV2('new-bucket', bucket=f'my-s3-new-bucket-{stack}')
 
-new_bucket_website = aws.s3.BucketWebsiteConfigurationV2('new_bucket',
-                                                        bucket=new_bucket.id,
-                                                        index_document={
-                                                            'suffix': 'index.html'
-                                                        })
-
-new_bucket_ownership_controls = aws.s3.BucketOwnershipControls('my-s3-bucket',
+new_bucket_ownership_controls = aws.s3.BucketOwnershipControls(f'{stack}-s3-ownership',
     bucket=new_bucket.id,
     rule={
         'object_ownership': 'BucketOwnerPreferred',
     })
 
-new_bucket_public_access = aws.s3.BucketPublicAccessBlock('my-s3-bucket',
+new_bucket_public_access = aws.s3.BucketPublicAccessBlock(f'{stack}-s3-accessblock',
                                                       bucket=new_bucket.id,
                                                       block_public_acls=False,
                                                       block_public_policy=False,
@@ -29,7 +27,7 @@ new_bucket_public_access = aws.s3.BucketPublicAccessBlock('my-s3-bucket',
                                                       restrict_public_buckets=False
                                                       )
 
-new_bucket_acl = aws.s3.BucketAclV2('my-s3-bucket',
+new_bucket_acl = aws.s3.BucketAclV2(f'{stack}-s3-acl',
                                 bucket=new_bucket.id,
                                 acl='public-read',
                                 opts=pulumi.ResourceOptions(depends_on=[
@@ -37,14 +35,21 @@ new_bucket_acl = aws.s3.BucketAclV2('my-s3-bucket',
                                     new_bucket_ownership_controls
                                 ]))
 
-filepath = os.path.join('sites', 'index.html')
-mime_type, _unneeded = mimetypes.guess_type(filepath)
-obj = aws.s3.BucketObject('index.html',
-                          bucket=new_bucket.id,
-                          source=pulumi.FileAsset(filepath),
-                          acl="public-read",
-                          content_type=mime_type
-                          )
+new_bucket_website = aws.s3.BucketWebsiteConfigurationV2(f'{stack}-s3-webconfig',
+                                                        bucket=new_bucket.id,
+                                                        index_document={
+                                                            'suffix': 'index.html'
+                                                        })
 
-pulumi.export('new_bucket_name', new_bucket.bucket)
-pulumi.export('new_bucket_endpoint', pulumi.Output.concat('http://', new_bucket_website.website_endpoint))
+for cfile in os.listdir(site_dir):
+    filepath = os.path.join(site_dir, cfile)
+    mime_type, _unneeded = mimetypes.guess_type(filepath)
+    obj = aws.s3.BucketObject(cfile,
+                            bucket=new_bucket.id,
+                            source=pulumi.FileAsset(filepath),
+                            acl="public-read",
+                            content_type=mime_type
+                            )
+
+pulumi.export(f'{stack}_bucket_name', new_bucket.bucket)
+pulumi.export(f'{stack}_bucket_endpoint', pulumi.Output.concat('http://', new_bucket_website.website_endpoint))
